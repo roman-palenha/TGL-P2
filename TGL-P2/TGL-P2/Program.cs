0385887f695e9a7f6a8b99fdea523a1e18a2e7ac
roman-palenha <roman.palenha04@gmail.com>
2022-07-17 18:16:55 +0300

    init

---
+++ TGL-P2/TGL-P2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TGL_P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
